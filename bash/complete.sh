bd2cc0212cbb9db335b8cb096adad8c52b2858bf
SahayanKY <a1c4f0x324@gmail.com>
2024-06-17 21:21:06 +0900

    fix: correct of completion contents

--- bash/complete.sh
+++ bash/complete.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 
 ###################################################################
-function __complete_files2files(){
-	local cur prev cword
-	_get_comp_words_by_ref -n : cur prev cword
-
-	if [[ "$cur" =~ ^- ]]; then
-		local totaloptionlist="${flagoptionlist[@]} ${valueoptionlist[@]} ${fileoptionlist[@]}"
-		if [ "$cword" -eq 1 ]; then
-			# add -o option
-			totaloptionlist="-o ${totaloptionlist}"
-		fi
-		COMPREPLY=( $(compgen -W "$totaloptionlist" -- "$cur") )
-	else
-		if [ "$prev" = -o ] && [ "$cword" -eq 2 ]; then
-			COMPREPLY=()
-		elif [[ " ${flagoptionlist[@]} " =~ \ ${prev}\  ]]; then
-			COMPREPLY=( $(compgen -f -- "$cur") )
-		elif [[ " ${valueoptionlist[@]} " =~ \ ${prev}\  ]]; then
-			COMPREPLY=()
-		elif [[ " ${fileoptionlist[@]} " =~ \ ${prev}\  ]]; then
-			COMPREPLY=( $(compgen -f -- "$cur") )
-		else
-			COMPREPLY=( $(compgen -f -- "$cur") )
-		fi
-	fi
+function __complete_file2seqfiles(){
+	__complete_files2files
+}
+function __complete_file2seqfiles_no_option(){
+	local flagoptionlist=(
+		-h --help
+	)
+	local valueoptionlist=()
+	local fileoptionlist=()
+	__complete_file2seqfiles
 }
 
+#################################
 function __complete_file2str(){
 	local cur prev cword
 	_get_comp_words_by_ref -n : cur prev cword
@@ -46,12 +33,17 @@ function __complete_file2str(){
 		fi
 	fi
 }
-
-function __complete_file2seqfiles(){
-	__complete_files2files
+function __complete_file2str_no_option(){
+	local flagoptionlist=(
+		-h --help
+	)
+	local valueoptionlist=()
+	local fileoptionlist=()
+	__complete_file2str
 }
 
-function __complete_str2file(){
+#################################
+function __complete_files2files(){
 	local cur prev cword
 	_get_comp_words_by_ref -n : cur prev cword
 
@@ -72,21 +64,36 @@ function __complete_str2file(){
 		elif [[ " ${fileoptionlist[@]} " =~ \ ${prev}\  ]]; then
 			COMPREPLY=( $(compgen -f -- "$cur") )
 		else
-			COMPREPLY=()
+			COMPREPLY=( $(compgen -f -- "$cur") )
 		fi
 	fi
 }
+function __complete_files2files_no_option(){
+	local flagoptionlist=(
+		-h --help
+	)
+	local valueoptionlist=()
+	local fileoptionlist=()
+	__complete_files2files
+}
 
+#################################
 function __complete_str2file(){
 	local cur prev cword
 	_get_comp_words_by_ref -n : cur prev cword
 
 	if [[ "$cur" =~ ^- ]]; then
 		local totaloptionlist="${flagoptionlist[@]} ${valueoptionlist[@]} ${fileoptionlist[@]}"
+		if [ "$cword" -eq 1 ]; then
+			# add -o option
+			totaloptionlist="-o ${totaloptionlist}"
+		fi
 		COMPREPLY=( $(compgen -W "$totaloptionlist" -- "$cur") )
 	else
-		if [[ " ${flagoptionlist[@]} " =~ \ ${prev}\  ]]; then
+		if [ "$prev" = -o ] && [ "$cword" -eq 2 ]; then
 			COMPREPLY=( $(compgen -f -- "$cur") )
+		elif [[ " ${flagoptionlist[@]} " =~ \ ${prev}\  ]]; then
+			COMPREPLY=()
 		elif [[ " ${valueoptionlist[@]} " =~ \ ${prev}\  ]]; then
 			COMPREPLY=()
 		elif [[ " ${fileoptionlist[@]} " =~ \ ${prev}\  ]]; then
@@ -96,6 +103,47 @@ function __complete_str2file(){
 		fi
 	fi
 }
+function __complete_str2file_no_option(){
+	local flagoptionlist=(
+		-h --help
+	)
+	local valueoptionlist=()
+	local fileoptionlist=()
+	__complete_str2file
+}
+
+#################################
+function __complete_str2str(){
+	local cur prev cword
+	_get_comp_words_by_ref -n : cur prev cword
+
+	if [[ "$cur" =~ ^- ]]; then
+		local totaloptionlist="${flagoptionlist[@]} ${valueoptionlist[@]} ${fileoptionlist[@]}"
+		COMPREPLY=( $(compgen -W "$totaloptionlist" -- "$cur") )
+	else
+		if [[ " ${flagoptionlist[@]} " =~ \ ${prev}\  ]]; then
+			COMPREPLY=()
+		elif [[ " ${valueoptionlist[@]} " =~ \ ${prev}\  ]]; then
+			COMPREPLY=()
+		elif [[ " ${fileoptionlist[@]} " =~ \ ${prev}\  ]]; then
+			COMPREPLY=( $(compgen -f -- "$cur") )
+		else
+			COMPREPLY=()
+		fi
+	fi
+}
+function __complete_str2str_no_option(){
+	local flagoptionlist=(
+		-h --help
+	)
+	local valueoptionlist=()
+	local fileoptionlist=()
+	__complete_str2str
+}
+
+###################################################################
+complete -F __complete_str2str_no_option atomnum2symb
+
 
 ###################################################################
 function __complete_csvsmiles2png(){
@@ -112,9 +160,13 @@ function __complete_csvsmiles2png(){
 	)
 	local fileoptionlist=()
 
-	__complete_files2files
+	__complete_file2seqfiles
 }
 complete -F __complete_csvsmiles2png csvsmiles2png
+
+
+###################################################################
+complete -F __complete_files2files_no_option fchk2json
 
 
 ###################################################################
@@ -166,6 +218,34 @@ complete -F __complete_g16log2value g16log2value
 
 
 ###################################################################
+complete -F __complete_files2files_no_option inpcrd2crd
+
+
+###################################################################
+complete -F __complete_files2files_no_option mden2csv
+
+
+###################################################################
+complete -F __complete_files2files_no_option mdout2csv
+
+
+###################################################################
+# no difine of mkresp
+
+
+###################################################################
+complete -F __complete_files2files_no_option pdb2csv
+
+
+###################################################################
+# no difine of plotmden
+
+
+###################################################################
+# no difine of rmexcept
+
+
+###################################################################
 function __complete_smiles2png(){
 	local flagoptionlist=(
 		-h --help
@@ -181,28 +261,27 @@ complete -F __complete_smiles2png smiles2png
 
 
 ###################################################################
-function __complete_chemscript_file(){
-	local cur prev cword
-	_get_comp_words_by_ref -n : cur prev cword
+function __complete_smiles2xyz(){
+	local flagoptionlist=(
+		-h --help
+		-n --num-threads
+		-c --num-confs
+	)
+	local valueoptionlist=()
+	local fileoptionlist=()
 
-	COMPREPLY=( $(compgen -f -- "$cur") )
+	__complete_str2file
 }
-complete -F __complete_chemscript_file fchk2json
-complete -F __complete_chemscript_file inpcrd2crd
-complete -F __complete_chemscript_file mden2csv
-complete -F __complete_chemscript_file mdout2csv
-complete -F __complete_chemscript_file mkresp
-complete -F __complete_chemscript_file pdb2csv
-complete -F __complete_chemscript_file xyz2pdb
-complete -F __complete_chemscript_file rmexcept
+
+complete -F __complete_smiles2xyz smiles2xyz
 
 
 ###################################################################
-function __complete_chemscript_void(){
-	COMPREPLY=()
-}
-complete -F __complete_chemscript_void atomnum2symb
-complete -F __complete_chemscript_void symb2atomnum
+complete -F __complete_str2str_no_option symb2atomnum
+
+
+###################################################################
+complete -F __complete_files2files_no_option xyz2pdb
 
 
 
