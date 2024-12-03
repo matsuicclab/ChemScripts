64a6a991019796fd02807107362049c96709e8a4
SahayanKY <a1c4f0x324@gmail.com>
2024-12-03 17:12:15 +0900

    feat: implement Molecule class

--- python/chemscripts/molecule.py
+++ python/chemscripts/molecule.py
@@ -1,4 +1,7 @@
 import numpy as np
+from rdkit import Chem
+
+from chemscripts.unit import checkValidUnit, getUnitConversionFactor
 
 class Molecule:
     def __init__(self, atomicnumList=None, xyzList=None, unit='Angstrom'):
@@ -9,7 +12,7 @@ class Molecule:
             raise ValueError('xyzList is None')
         if unit is None:
             raise ValueError('unit is None')
-        
+
         # atomicnumListチェック
         if type(atomicnumList) in [list, tuple]:
             # listかtupleならnp.ndarrayに変換
@@ -21,7 +24,7 @@ class Molecule:
             raise TypeError('elements of atomicnumList must be positive, and the type must be int')
         if len(atomicnumList.shape) != 1:
             raise ValueError('shape of atomicnumList must be (*,)')
-        
+
         # xyzListチェック
         if type(xyzList) in [list, tuple]:
             # listかtupleならnp.ndarrayに変換
@@ -32,38 +35,62 @@ class Molecule:
             raise TypeError('dtype of xyzList must be float64')
         if len(xyzList.shape) != 2 or xyzList.shape[1] != 3:
             raise ValueError('shape of xyzList must be (*,3)')
-        
+
         # 要素数は一致しているか
         if len(atomicnumList) != len(xyzList):
             raise ValueError('The number of atoms differs between atomicnumList and xyzList')
-        
-        # unitチェック
-        if unit in ['Angstrom', 'A', 'Ang']:
-            unit = 'Angstrom'
-        elif unit in ['Bohr', 'a.u.']:
-            unit = 'Bohr'
-        else:
-            raise ValueError('invalid unit: {}'.format(unit))
-        
+
+        if checkValidUnit(unit):
+            raise ValueError('Invalid unit: {}'.format(unit))
+
         # メンバ変数に追加
         self.__numAtom = len(atomicnumList)
-        self.__atomicnumList = atomicnumList
-        self.__xyzList = xyzList
+        self.__atomicnumArray = atomicnumList
+        self.__xyzArray = xyzList
         self.__unit = unit
-        
-    def __giveUnitConvFactor(self, newunit):
-        return None
-        
+
     def giveNumAtom(self):
         return self.__numAtom
-        
-    def iterateAtom(self, unit='Angstrom', elementSymbol=True):
-        return None
-    
-    def generateRDKitMol(self):
-        #from rdkit import Chem
-        return None
-    
-    def giveXYZBlock(self, unit='Angstrom'):
-        return None
-    
+
+    def iterateAtoms(self, unit='Angstrom', elementSymbol=True):
+        factor = getUnitConversionFactor(self.__unit, unit)
+
+        numlist = self.__atomicnumArray # shape: (n,)
+        xyzlist = (self.__xyzArray * factor).T # shape: (3,n)
+
+        if elementSymbol:
+            table = Chem.GetPeriodicTable()
+            # convert atomic number to element symbol
+            symblist = [table.GetElementSymbol(int(n)) for n in numlist]
+
+            return zip(symblist, *xyzlist) # shape: (n,4)
+        else:
+            return zip(numlist, *xyzlist) # shape: (n,4)
+
+    def generateRDKitMolObj(self):
+        """
+        Requires RDKit 2022.09 or higher
+        """
+        from rdkit.Chem import rdDetermineBonds
+
+        xyzblock = self.giveXYZBlock(unit='Angstrom', elementSymbol=True)
+        mol = Chem.MolFromXYZBlock(xyzblock)
+        rdDetermineBonds.DetermineBonds(mol)
+        return mol
+
+    def giveXYZBlock(self, unit='Angstrom', elementSymbol=True):
+        result = [str(self.__numAtom), 'comment']
+        result.extend(
+            ['{} {} {} {}'.format(s,x,y,z) for s, x, y, z in self.iterateAtoms(unit=unit, elementSymbol=elementSymbol)]
+        )
+        result = '\n'.join(result)
+
+        return result
+
+
+
+
+
+
+
+
