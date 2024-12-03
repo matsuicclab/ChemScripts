a2d09dc5d875621d9faaf641012df03969822e5a
SahayanKY <a1c4f0x324@gmail.com>
2024-12-03 17:11:58 +0900

    feat: implement the factor

--- python/chemscripts/unit.py
+++ python/chemscripts/unit.py
@@ -1,22 +1,23 @@
 
 def __factor(unit):
-    
     if unit in ['Angstrom', 'A', 'Ang']:
-        # TODO fix
-        return 1
+        return 0.529177210544
     elif unit in ['Bohr', 'a.u.']:
         return 1
     else:
         raise ValueError('invalid unit: {}'.format(unit))
 
-def conversionFactor(oldunit=None, newunit=None):
+def checkValidUnit(unit):
+    return unit in ['Angstrom', 'A', 'Ang', 'Bohr', 'a.u.']
+
+def getUnitConversionFactor(oldunit=None, newunit=None):
     if oldunit is None:
         raise ValueError('oldunit is None')
     if newunit is None:
         raise ValueError('newunit is None')
-    
+
     oldfactor = __factor(oldunit)
     newfactor = __factor(newunit)
-    
+
     return newfactor / oldfactor
-    
+
