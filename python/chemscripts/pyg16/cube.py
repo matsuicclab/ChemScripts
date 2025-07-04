fix: fix the ESP calculation

python/chemscripts/pyg16/cube.py
@@ -457,12 +457,15 @@ class CubeGrid:
             self.__init__fromMoleculeObj(**args)
         elif 'startingPoint' in args.keys():
             self.__init__fromParam(**args)
+        elif 'cubeGrid' in args.keys():
+            self.__init__fromCubeGrid(**args)
         else:
-            raise ValueError('args must contain moleculeObj or startingPoint')
+            raise ValueError('args must contain moleculeObj or startingPoint or cubeGrid')
 
     def __init__fromMoleculeObj(self, moleculeObj=None, axesMethod=None, step=0.5, padding=3.0, unit='Angstrom'):
         """
         unit: unit of step and padding
+        axesMethod: 'Direct' or 'PCA' or 'PCA-ignoreHs'
         """
 
         # molecule
@@ -612,6 +615,46 @@ class CubeGrid:
         self.__numGridPoint = numGridPoint
         self.__unit = unit
 
+    def __init__fromCubeGrid(self, cubeGrid=None, stepRatio=1, numMarginGrid=0, shiftGrid=0):
+        """
+        stepRatio: 非ゼロ整数
+                    newStepVector = oldStepVector * abs(ratio) ** sign(ratio)
+        numMarginGrid: 整数 
+                    与えられたcubeGridの外側にどれだけ広げるか(newStepVector単位)
+        shiftGrid: [0,1)の実数
+                    startingPointをどれだけズラすか(newStepVector単位)
+        """
+        if type(cubeGrid) is not CubeGrid:
+            raise TypeError('type of cubeGrid must be CubeGrid')
+        if type(stepRatio) is not int:
+            raise TypeError('type of stepRatio must be int')
+        if type(numMarginGrid) is not int:
+            raise TypeError('type of numMarginGrid must be int')
+        if type(shiftGrid) not in [int, float]:
+            raise TypeError('shiftGrid must be a number')
+
+        if stepRatio == 0:
+            raise ValueError('stepRatio must not be zero')
+        if shiftGrid < 0 or shiftGrid >= 1:
+            raise ValueError('shiftGrid must be a real number greater than or equal to 0 and less than 1')
+        
+        # 単位決定
+        newUnit = cubeGrid.__unit
+        
+        # stepVector決定
+        ratio = np.abs(stepRatio) ** np.sign(stepRatio)
+        newStepVector = cubeGrid.__stepVector * ratio
+        v1, v2, v3 = newStepVector
+        
+        # numGridPoint決定
+        newNumGridPoint = cubeGrid.__numGridPoint + numMarginGrid * 2
+        
+        # startingPoint決定
+        newStartingPoint = cubeGrid.__startingPoint + (v1+v2+v3) * (- numMarginGrid + shiftGrid)
+
+        # 構築        
+        self.__init__fromParam(startingPoint=newStartingPoint, stepVector=newStepVector, unit=unit)
+        
 
     def __str__(self):
         return '\n'.join([

python/chemscripts/pyg16/fchk.py
@@ -414,6 +414,7 @@ class Fchk:
     def generateElectronDensityCube(self, step=0.2, padding=3.0, unit='Angstrom', cubeGrid=None):
         """
         電子密度のcubeデータを生成
+        unit: step, paddingの単位指定 (cubeGrid指定時は無視)
         return: Cubeインスタンス
         """
         if cubeGrid is not None:
@@ -439,9 +440,12 @@ class Fchk:
             return self.generateElectronDensityCube(cubeGrid=cubeGrid)
 
 
-    def generateElectrostaticPotentialCube(self, step=0.2, padding=3.0, unit='Angstrom', cubeGrid=None, densCube=None):
+    def generateElectrostaticPotentialCube(self, step=0.2, padding=3.0, unit='Angstrom', espunit='a.u.', cubeGrid=None, densDetailRatio=1):
         """
         静電ポテンシャルのcubeデータを生成
+        unit: step, paddingの単位指定 (cubeGrid指定時は無視)
+        densDetailRatio: 非ゼロ整数
+                    densStepVector = espStepVector * abs(ratio) ** sign(ratio)
         return: Cubeインスタンス
         """
         molecule = self.giveMoleculeObj()
@@ -450,39 +454,43 @@ class Fchk:
             # cubeGridが指定されている場合
             if type(cubeGrid) is not CubeGrid:
                 raise TypeError('type of cubeGrid must be chemscript.pyg16.cube.CubeGrid')
+            
+            if densDetail is not int:
+                raise TypeError('type of densDetail must be int')
 
-            if densCube is None:
-                raise ValueError('densCube is None')
-            if type(densCube) is not Cube:
-                raise TypeError('type of densCube must be chemscript.pyg16.cube.Cube')
+            unit = 'Bohr' # 一旦a.u.で計算する
 
-            unit = 'Bohr'
-
+            # 電子密度分布のグリッドを生成
+            # ESPグリッドから等距離に電子密度グリッドを配置するように位置を調整(shift=0.5)
+            # これをしないと二つの離散点が重なってしまい発散してしまう
+            densCubeGrid = CubeGrid(cubeGrid=cubeGrid, stepRatio=densDetailRatio, numMarginGrid=2, shiftGrid=0.5)
+            
             # 電子密度分布を取得
-            dens = densCube.giveCubeData() # shape: (na2,nb2,nc2,1)
-            coords_dens = densCube.giveNodeCoord(unit=unit) # shape: (na2,nb2,nc2,3)
+            coords_dens = densCubeGrid.giveNodeCoord(unit=unit).reshape(-1,3) # shape: (na1*nb1*nc1,3)
+            dens = self.calcElectronDensity(coords_dens) # shape: (na1*nb1*nc1,)
 
             # ポテンシャルの計算点の座標を取得
-            coords_pot = cubeGrid.giveNodeCoord(unit=unit) # shape: (na1,nb1,nc1,3)
-            numgrid_pot = cubeGrid.giveNumGridPoint() # == (na1,nb1,nc1)
+            coords_pot = cubeGrid.giveNodeCoord(unit=unit) # shape: (na2,nb2,nc2,3)
 
             # 電子由来の静電ポテンシャル
             # 一気に距離行列を計算するとメモリオーバーになる可能性があるため、
             # ポテンシャルの計算点ごとに計算を実行
-            dens = dens.reshape(-1)
-            coords_dens = coords_dens.reshape(-1,3)
-            pot_el = (-1) * np.array([np.sum(np.linalg.norm(coords_dens-r,axis=1) * dens) for r in coords_pot.reshape(-1,3)]).reshape(*numgrid_pot,1) # shape: (na1,nb1,nc1,1), unit: a.u.
+            pot_el = (-1) * np.array([np.sum(dens / np.linalg.norm(coords_dens-r,axis=1)) for r in coords_pot.reshape(-1,3)]) # shape: (na2*nb2*nc2,), unit: a.u.
 
             # 原子核由来の静電ポテンシャル
             atomicnums = np.array(molecule.giveAtomicnumList()) # shape: (numAtom,)
             atomXYZArray = molecule.giveXYZArray(unit=unit) # shape: (numAtom, 3)
-            pot_nu = np.sum(atomicnums * cdist(coords_pot.reshape(-1,3), atomXYZArray), axis=0).reshape(*numgrid_pot,1) # shape: (na1,nb1,nc1,1), unit: a.u.
+            pot_nu = np.sum(atomicnums / cdist(coords_pot.reshape(-1,3), atomXYZArray), axis=1) # shape: (na2*nb2*nc2,), unit: a.u.
 
             # 足し算
             pot = pot_el + pot_nu
 
+            # 単位変換
+            if espunit == 'V':
+                pot *= 27.21162
+
             # Cubeインスタンス生成
-            cube = Cube(cubeGrid=cubeGrid, cubeData=pot, valueNames=['ElectrostaticPotential'], moleculeObj=molecule)
+            cube = Cube(cubeGrid=cubeGrid, cubeData=pot, valueNames=['ElectrostaticPotential[{}]'.format(espunit)], moleculeObj=molecule)
 
             return cube
 
@@ -491,7 +499,7 @@ class Fchk:
             cubeGrid = CubeGrid(moleculeObj=molecule, axesMethod='Direct', step=step, padding=padding, unit=unit)
             
             # 再度呼び出し
-            return self.generateElectrostaticPotentialCube(cubeGrid=cubeGrid, densCube=densCube)
+            return self.generateElectrostaticPotentialCube(cubeGrid=cubeGrid, densDetail=densDetail)
 
 
 